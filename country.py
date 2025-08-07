4839044486d1853e9136f3b958135382f3b515b7
Thom Null <raulesquerra18@gmail.com>
2025-08-06 20:51:59 -0700

    implemented boilerplate equality n hash dunders to be able to convert repeated country objects from list to a set. Passed in DictReader dict keys as arguments for country object constructor in main.py

--- country.py
+++ country.py
@@ -1,6 +1,5 @@
 # this file contains the construction of a country and its attributes
 # attributes: Country Name,Country Code,Series Name,Series Code,Year
-
 class Country:
 
     def __init__(self, country_name, country_code, series_name, series_code):
@@ -34,10 +33,23 @@ class Country:
         self._series_code = series_code
     
     def __str__(self):
-        return (f"Country Name: {self.get_country_name}, Country Code: {self.get_country_code}, Series Name: {self.get_series_name}, Series Code: {self.get_series_code}")
+        return (f"Country Name: {self.get_country_name()}, Country Code: {self.get_country_code()}, Series Name: {self.get_series_name()}, Series Code: {self.get_series_code()}")
 
+    # allows the comparison between country objects to produce a valid bool instead of defaulting to False
+    # for creating a set out of list of countries in main.py
     def __eq__(self, value):
-        pass
+        if not isinstance(value, Country):
+            return NotImplemented
+        return (self._country_name == value._country_name and 
+                self._country_code == value._country_code and
+                self._series_name == value._series_name and
+                self._series_code == value._series_code)
     
+    # must be used in conjuction with equality dunder: allows country objects to be usable keys in dictionaries or elems in sets
+    # for creating a set out of list of countries in main.py
     def __hash__(self):
-        pass 
+        return hash((
+            self._country_name,
+            self._country_code,
+            self._series_name,
+            self._series_code))
--- main.py
+++ main.py
@@ -7,29 +7,23 @@ def main():
         
         if not reader.fieldnames:
             print("no data")
+            return
 
         bulk_countries = []
     
         for line in reader:
-
-            country = Country(None, None, None, None)
-    
-            country.set_country_name(line['Country Name'])
-            country.set_country_code(line['Country Code'])
-            country.set_series_name(line['Series Name'])
-            country.set_series_code(line['Series Code'])
-            # country.set_country_name(line['Year'])
+            country = Country(
+                line['Country Name'], 
+                line['Country Code'], 
+                line['Series Name'], 
+                line['Series Code'])
         
             bulk_countries.append(country)
         
         countries = set(bulk_countries)
-        # print(bulk_countries)
-        # print("****")
 
         for country in countries:
             print(country)
-
-        file.close()
 
 if __name__ == "__main__":
     main()
