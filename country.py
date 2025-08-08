f33df5f0779b4cf20693d6af6f8ff265f9e8f5f3
Thom Null <raulesquerra18@gmail.com>
2025-08-08 09:05:11 -0700

    updated main.py display with added data DictReader field. Updated csv files to include data for that given year to output. Added data to country object constructor in country.py

--- country.py
+++ country.py
@@ -2,11 +2,12 @@
 # attributes: Country Name,Country Code,Series Name,Series Code,Year
 class Country:
 
-    def __init__(self, country_name, country_code, series_name, series_code):
+    def __init__(self, country_name, country_code, series_name, series_code, data):
         self._country_name = country_name
         self._country_code = country_code
         self._series_name = series_name
         self._series_code = series_code
+        self._data = data
 
     def get_country_name(self):
         return self._country_name
@@ -31,9 +32,18 @@ class Country:
     
     def set_series_code(self, series_code):
         self._series_code = series_code
+
+    def get_data(self):
+        return self._data
+    
+    def set_data(self, data):
+        if data:
+            self._data = data
+        else:
+            self._data = 0
     
     def __str__(self):
-        return (f"Country Name: {self.get_country_name()}, Country Code: {self.get_country_code()}, Series Name: {self.get_series_name()}, Series Code: {self.get_series_code()}")
+        return (f"Country Name: {self.get_country_name()}, Country Code: {self.get_country_code()}, Series Name: {self.get_series_name()}, Series Code: {self.get_series_code()}, Data: {self.get_data()}")
 
     # allows the comparison between country objects to produce a valid bool instead of defaulting to False
     # for creating a set out of list of countries in main.py
@@ -43,7 +53,8 @@ class Country:
         return (self._country_name == value._country_name and 
                 self._country_code == value._country_code and
                 self._series_name == value._series_name and
-                self._series_code == value._series_code)
+                self._series_code == value._series_code and
+                self._data == value._data)
     
     # must be used in conjuction with equality dunder: allows country objects to be usable keys in dictionaries or elems in sets
     # for creating a set out of list of countries in main.py
@@ -52,4 +63,5 @@ class Country:
             self._country_name,
             self._country_code,
             self._series_name,
-            self._series_code))
+            self._series_code,
+            tuple(self._data) if self._data else None))
--- main.py
+++ main.py
@@ -16,14 +16,18 @@ def main():
                 line['Country Name'], 
                 line['Country Code'], 
                 line['Series Name'], 
-                line['Series Code'])
+                line['Series Code'],
+                line['2020 [YR2020]'])
         
             bulk_countries.append(country)
         
-        countries = set(bulk_countries)
+        # countries = set(bulk_countries)
+        
+        for i in range(len(bulk_countries)):
+            print(bulk_countries[i].get_country_name() + ":", bulk_countries[i].get_series_name() + ":", bulk_countries[i].get_data())
 
-        for country in countries:
-            print(country)
+        # for country in countries:
+        #     print(country)
 
 if __name__ == "__main__":
     main()
